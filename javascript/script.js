e750570d60de0680f3b036f301da8bdbfc6b7c12
gh-ashkan <ashkanghavami8@gmail.com>
2024-02-18 22:15:02 +0330

    add section flash sale`s

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,23 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper0 = new Swiper(".s0", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper1 = new Swiper(".s1", {
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      // el: ".swiper-pagination",
+      type: "fraction",
     },
     navigation: {
       nextEl: ".swiper-button-next",
